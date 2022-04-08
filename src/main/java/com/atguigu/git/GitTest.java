28552dfa77930a7cc2418e78e1b0b03a98c788a5
hallo <hallo@gmail.com>
2022-04-08 16:41:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hallo
+ * @createtime 2022-04-08 16:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
